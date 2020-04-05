e94f9e0fb7cfc5771e0ccfc28d35020b2ebddc82
cindy-lihan <cindylee1224@163.com>
2020-04-05 10:34:31 +0800

    添加eventHub

---
+++ src/js/event-hub.js
@@ -0,0 +1,27 @@
+window.event = {
+    events:{},
+    emit(eventName, data){  //发布
+        for(let key in this.events){
+            if(key === eventName){
+                let fnList = this.events[key]  
+                fnList.map((fn) => {
+                    fn.call(undefined, data)
+                })   
+            }
+        }
+    },
+    on(eventName, fn){ //订阅
+        for(let key in this.events){
+            if(key === eventName){
+                if(this.events[key] === undefined){
+                    this.events[key] = []
+                }
+                this.events[key].push(fn)
+            }
+        }
+
+    },
+    off(){
+
+    }
+}
