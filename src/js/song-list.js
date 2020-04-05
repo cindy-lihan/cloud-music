187207ce4ea01edf1705ba95ec4e8189b69000d2
cindy-lihan <cindylee1224@163.com>
2020-04-05 10:33:49 +0800

    模块化

---
+++ src/js/song-list.js
@@ -0,0 +1,54 @@
+{
+  let view = {
+    el: "#songlist-container",
+    template: `
+        <div class="flex-list-header">
+        <div class="flex-row">
+            <div class="flex-cell">歌名</div>
+            <div class="flex-cell">歌手</div>
+            <div class="flex-cell">专辑</div>
+            <div class="flex-cell">链接</div>
+            <div class="flex-cell">操作</div>
+        </div>
+    </div>
+    <div class="flex-list-item first">
+        <div class="flex-row content">
+            <div class="flex-cell first name">歌名xxx</div>
+            <div class="flex-cell singer">歌手xxx</div>
+            <div class="flex-cell collection">专辑xxx</div>
+            <div class="flex-cell url">外链x</div>
+            <div class="flex-cell operating">
+                <a href="javascript:void(0);" class="cell-edit">修改</a>
+                <a href="javascript:void(0);" class="cell-delete">删除</a>
+            </div>
+        </div>
+    </div>
+    <div class="flex-list-item">
+        <div class="flex-row content">
+            <div class="flex-cell name">歌名yyy</div>
+            <div class="flex-cell singer">歌手yyy</div>
+            <div class="flex-cell collection">专辑yyy</div>
+            <div class="flex-cell url">外链y</div>
+            <div class="flex-cell operating">
+                <a href="javascript:void(0);" class="cell-edit">修改</a>
+                <a href="javascript:void(0);" class="cell-delete">删除</a>
+            </div>
+        </div>
+    </div>
+    `,
+    render(data) {
+      $(this.el).html(this.template);
+    }
+  };
+
+  let model = {};
+  let controller = {
+    init(view, model) {
+      this.view = view;
+      this.model = model;
+      this.view.render(this.model.data);
+    }
+  };
+
+  controller.init(view, model);
+}
---
+++ src/js/upload-song.js
@@ -0,0 +1,67 @@
+{
+  let view = {
+    el: ".upload-pop-root",
+    find: function(selector) {
+      return $(this.el).find(selector)[0];
+    }
+  };
+  let model = {};
+  let controller = {
+    init(view, model) {
+      this.view = view;
+      this.model = model;
+      this.initQiniu();
+    },
+    initQiniu() {
+      //引入Plupload 、qiniu.js后
+      var uploader = Qiniu.uploader({
+        runtimes: "html5,flash,html4", //上传模式,依次退化
+        browse_button: this.view.find('#upload-button'), //上传选择的点选按钮，**必需**
+        uptoken_url: "http://localhost:19110/uptoken", //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
+        domain: "q7soun4xa.bkt.clouddn.com", //bucket 域名，下载资源时用到，**必需**
+        get_new_uptoken: false, //设置上传文件的时候是否每次都重新获取新的token
+        unique_names: false,
+        container: this.view.find('#upload-container'), //上传区域DOM ID，默认是browser_button的父元素，
+        max_file_size: "20mb", //最大文件体积限制
+        dragdrop: true, //开启可拖曳上传
+        drop_element:  this.view.find('#upload-area'), //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+        chunk_size: "4mb", //分块上传时，每片的体积
+        auto_start: true, //选择文件后自动上传，若关闭需要自己绑定事件触发上传
+        init: {
+          FilesAdded: function(up, files) {
+            plupload.each(files, function(file) {
+              // 文件添加进队列后,处理相关的事情
+            });
+          },
+          BeforeUpload: function(up, file) {
+            // 每个文件上传前,处理相关的事情
+          },
+          UploadProgress: function(up, file) {
+            // 每个文件上传时,处理相关的事情
+          },
+          FileUploaded: function(up, file, info) {
+            var domain = up.getOption("domain");
+            var res = JSON.parse(info.response);
+            var sourceLink =
+              "http://" + domain + "/" + encodeURIComponent(res.key);
+          },
+          Error: function(up, err, errTip) {
+            //上传出错时,处理相关的事情
+          },
+          UploadComplete: function() {
+            //队列文件处理完毕后,处理相关的事情
+          },
+          Key: function(up, file) {
+            // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+            // 该配置必须要在 unique_names: false , save_key: false 时才生效
+
+            var key = "";
+            // do something with key here
+            return key;
+          }
+        }
+      });
+    }
+  };
+  controller.init(view, model);
+}
